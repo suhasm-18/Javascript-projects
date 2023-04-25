4e542146995a7f614a792c95ea7f8222ba72d943
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-04-25 19:48:17 +0530

    Update index.js

--- Project-03/index.js
+++ Project-03/index.js
@@ -2,3 +2,18 @@ const btnEl = document.querySelector(".btn");
 const inputEl = document.querySelector("input");
 const messageEl = document.querySelector(".message");
 const errorEl = document.querySelector(".error");
+
+btnEl.addEventListener("click",displayMessage);
+
+function displayMessage(){
+    if(inputEl.value){
+        messageEl.textContent = inputEl.value;
+        inputEl.value = "";
+    }else{
+        errorEl.style.display = "block";
+       setInterval(() => {
+        errorEl.style.display = "none";
+       }, 10000);
+    }
+    
+}
