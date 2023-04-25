d5b7f72d8dfb13457f63ede8760ec31cf2f75bd5
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-04-25 19:48:00 +0530

    Create index.js

---
+++ Project-03/index.js
@@ -0,0 +1,4 @@
+const btnEl = document.querySelector(".btn");
+const inputEl = document.querySelector("input");
+const messageEl = document.querySelector(".message");
+const errorEl = document.querySelector(".error");
