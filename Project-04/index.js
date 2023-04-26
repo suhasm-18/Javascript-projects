750d88549cbae994295e5e7575d7db10ddc678a3
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-04-26 18:36:11 +0530

    Create index.js

---
+++ Project-04/index.js
@@ -0,0 +1,22 @@
+const prevEl = document.querySelector(".dec")
+const nextEl = document.querySelector(".inc")
+const resetEl = document.querySelector(".reset");
+const buttons = document.querySelector(".btn")
+let countEl = document.querySelector(".count");
+
+let counts = 0;
+
+prevEl.addEventListener("click",decrease);
+nextEl.addEventListener("click",increase);
+resetEl.addEventListener("click",reset);
+
+function increase(){
+    counts++;
+    countEl.innerHTML = counts;
+    if(counts>0){
+        countEl.style.color = "green";
+    }
+    if(counts==0){
+        countEl.style.color = "black";
+    }
+}
