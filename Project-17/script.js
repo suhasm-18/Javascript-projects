c70371995cce4f92f1ef9e1ad41c87d407d8c787
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-06-22 19:28:13 +0530

    Create script.js

---
+++ Project-17/script.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('.button').forEach(button => {
+
+    let duration = 3000,
+        svg = button.querySelector('svg'),
+        svgPath = new Proxy({
+            y: null,
+            smoothing: null
+        }, {
+            set(target, key, value) {
+                target[key] = value;
+                if(target.y !== null && target.smoothing !== null) {
+                    svg.innerHTML = getPath(target.y, target.smoothing, null);
+                }
+                return true;
+            },
+            get(target, key) {
+                return target[key];
+            }
+        });
